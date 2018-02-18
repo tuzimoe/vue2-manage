a6baae1714f94b15b5c773e2c9b408f757cad6a5
兔子鲜笙 <a@tuzi.moe>
2018-02-18 18:03:51 +1300

    local

--- src/config/env.js
+++ src/config/env.js
@@ -1,18 +1,18 @@
 /**
  * 配置编译环境和线上环境之间的切换
- * 
+ *
  * baseUrl: 域名地址
  * routerMode: 路由模式
  * baseImgPath: 图片存放地址
- * 
+ *
  */
-let baseUrl = ''; 
+let baseUrl = '';
 let routerMode = 'history';
 let baseImgPath;
 
 if (process.env.NODE_ENV == 'development') {
-	  baseUrl = 'http://localhost:8002';
-    baseImgPath = 'http://localhost:8002/img/';
+	  baseUrl = 'http://wel.tuzi.moe:8001';
+    baseImgPath = 'http://wel.tuzi.moe:8001/img/';
 }else{
 	  baseUrl = 'http://cangdu.org:8001';
     baseImgPath = 'http://cangdu.org:8001/img/';
